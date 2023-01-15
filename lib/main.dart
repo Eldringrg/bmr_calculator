4773892ebf00d1956fb20dd5209953c32c037d75
Eldringrg <grg.aldrin@ymail.com>
2023-01-15 13:18:49 +0545

    Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ void main() {
 }
 
 class BMR extends StatelessWidget {
+  int _val;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Container(
@@ -17,9 +19,131 @@ class BMR extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
+          backgroundColor: Color.fromARGB(255, 2, 37, 102),
         ),
-        body: Card(
-          child: TextField(),
+        body: SingleChildScrollView(
+          child: Card(
+            elevation: 10,
+            child: Container(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Age',
+                            textScaleFactor: 2,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'ages 15-80',
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Gender',
+                            textScaleFactor: 2,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Radio(
+                            value: 1,
+                            groupValue: _val,
+                            onChanged: (value) {
+                              value = _val;
+                            },
+                          ),
+                        ),
+                        Text(
+                          'Male',
+                        ),
+                        Radio(
+                          value: 2,
+                          groupValue: _val,
+                          onChanged: (value) {
+                            value = _val;
+                          },
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Female',
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Height',
+                            textScaleFactor: 2,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Weight',
+                            textScaleFactor: 2,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        'Calculate',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                      color: Colors.green,
+                    ),
+                  ]),
+            ),
+          ),
         ),
       ),
     );
