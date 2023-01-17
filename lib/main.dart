f3c207d5e8367cbe9c5a0299ea316cc3bf873001
Eldringrg <grg.aldrin@ymail.com>
2023-01-17 20:38:36 +0545

    Do correction

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,37 @@ void main() {
   runApp(BMR());
 }
 
-class BMR extends StatelessWidget {
+class BMR extends StatefulWidget {
+  @override
+  State<BMR> createState() => _BMRState();
+}
+
+class _BMRState extends State<BMR> {
+  TextEditingController Age = TextEditingController();
+
+  TextEditingController Weight = TextEditingController();
+
+  TextEditingController Height = TextEditingController();
+
   int _val;
+  int age = 0;
+  int height = 0;
+  int weight = 0;
+
+  double result;
+  basalmetabolismrate() {
+    setState(
+      () {
+        age = int.parse(Age.text);
+        weight = int.parse(Height.text);
+        height = int.parse(Weight.text);
+
+        result =
+            (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age));
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,32 +43,39 @@ class BMR extends StatelessWidget {
         appBar: AppBar(
           title: Container(
             width: double.infinity,
-            child: Text(
-              'BMR Calculator',
+            child: const Text(
+              'BMR CALCULATOR',
               textAlign: TextAlign.center,
             ),
           ),
           backgroundColor: Color.fromARGB(255, 2, 37, 102),
         ),
         body: SingleChildScrollView(
-          child: Card(
-            elevation: 10,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              child: Column(
+          child: Container(
+            margin: EdgeInsets.only(
+              top: 50,
+              left: 10,
+              right: 10,
+            ),
+            child: Card(
+              elevation: 10,
+              child: Container(
+                padding: EdgeInsets.all(30),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
                     Row(
                       children: <Widget>[
                         Container(
                           padding: EdgeInsets.all(10),
-                          child: Text(
+                          child: const Text(
                             'Age',
                             textScaleFactor: 2,
                           ),
                         ),
                         Expanded(
                           child: TextField(
+                            controller: Age,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
                             ),
@@ -48,7 +84,7 @@ class BMR extends StatelessWidget {
                         ),
                         Container(
                           padding: EdgeInsets.all(10),
-                          child: Text(
+                          child: const Text(
                             'ages 15-80',
                           ),
                         ),
@@ -58,7 +94,7 @@ class BMR extends StatelessWidget {
                       children: <Widget>[
                         Container(
                           padding: EdgeInsets.all(10),
-                          child: Text(
+                          child: const Text(
                             'Gender',
                             textScaleFactor: 2,
                           ),
@@ -73,7 +109,7 @@ class BMR extends StatelessWidget {
                             },
                           ),
                         ),
-                        Text(
+                        const Text(
                           'Male',
                         ),
                         Radio(
@@ -85,7 +121,7 @@ class BMR extends StatelessWidget {
                         ),
                         Container(
                           padding: EdgeInsets.all(10),
-                          child: Text(
+                          child: const Text(
                             'Female',
                           ),
                         ),
@@ -95,15 +131,17 @@ class BMR extends StatelessWidget {
                       children: <Widget>[
                         Container(
                           padding: EdgeInsets.all(10),
-                          child: Text(
+                          child: const Text(
                             'Height',
                             textScaleFactor: 2,
                           ),
                         ),
                         Expanded(
                           child: TextField(
+                            controller: Height,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
+                              hintText: 'in cm',
                             ),
                             keyboardType: TextInputType.number,
                           ),
@@ -113,17 +151,19 @@ class BMR extends StatelessWidget {
                     Row(
                       children: <Widget>[
                         Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
+                          margin: EdgeInsets.only(top: 10),
+                          padding: EdgeInsets.all(20),
+                          child: const Text(
                             'Weight',
                             textScaleFactor: 2,
                           ),
                         ),
                         Expanded(
                           child: TextField(
+                            controller: Weight,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
+                              hintText: 'in kg',
                             ),
                             keyboardType: TextInputType.number,
                           ),
@@ -138,10 +178,26 @@ class BMR extends StatelessWidget {
                           color: Colors.white,
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        basalmetabolismrate();
+                        Age.clear();
+                        Weight.clear();
+                        Height.clear();
+                      },
                       color: Colors.green,
                     ),
-                  ]),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'Result:$result' + 'Calories/day',
+                        style: TextStyle(),
+                        textAlign: TextAlign.center,
+                        textScaleFactor: 1.6,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
